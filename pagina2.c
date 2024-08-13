f654371852722483e8b48e8b58d2c4f398a4908c
mendezgarabetti <31359723+mendezgarabetti@users.noreply.github.com>
2024-08-13 01:45:28 -0300

    Create pagina2.c

---
+++ pagina2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Función principal donde comienza la ejecución del programa
+int main() {
+    // Declaración de una variable entera
+    int numero = 10;
+
+    // Imprimir el valor de la variable
+    printf("El valor de la variable numero es: %d\n", numero);
+
+    // Fin de la función main
+    return 0;
+}
